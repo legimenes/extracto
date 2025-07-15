9decfe60189a837f8b7b438404620162c19f10ed
Leandro G <leandru@gmail.com>
2025-07-15 12:48:20 -0300

    feat: creating expressions grid new layout

--- frontend/src/components/activities/ActivityModal.tsx
+++ frontend/src/components/activities/ActivityModal.tsx
@@ -149,26 +149,47 @@ const ActivityModal = ({ id, open, onClose, onSave }: ActivityModalProps) => {
                 </button>
               </div>
             </form>
-            {id !== null && activity?.expressions && activity.expressions.length > 0 && (
+            {id !== null && (
               <div className="mt-6">
-                <div className="overflow-auto max-h-48">
-                  <table className="min-w-full bg-neutral-900 border-collapse border border-neutral-700 select-none">
-                    <thead className="sticky top-0">
-                      <tr className="bg-neutral-900 text-neutral-400">
-                        <th className="pl-2 text-start cursor-pointer">Expressões</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {activity.expressions.map((expression) => (
-                        <tr
-                          key={expression.id}
-                          className="border border-neutral-700 hover:bg-neutral-800">
-                          <td className="p-2">{expression.pattern}</td>
+                {activity?.expressions && activity.expressions.length > 0 ? (
+                  <div className="overflow-auto max-h-48">
+                    <table className="min-w-full bg-neutral-900 border-collapse border border-neutral-700 select-none">
+                      <thead className="sticky top-0">
+                        <tr className="bg-neutral-900 text-neutral-400">
+                          <th className="pl-2 text-start cursor-pointer">
+                            <button
+                              className="px-3 py-1 text-sm text-white font-semibold rounded bg-lime-600 hover:bg-lime-700"
+                              //onClick={handleOpenExpressionModal}
+                              disabled={loading}>
+                              +
+                            </button>
+                            &nbsp;Expressão
+                          </th>
+                          <th></th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
+                      </thead>
+                      <tbody>
+                        {activity.expressions.map((expression) => (
+                          <tr
+                            key={expression.id}
+                            className="border border-neutral-700 hover:bg-neutral-800">
+                            <td className="p-2">{expression.pattern}</td>
+                            <td className="p-2 text-right">
+                              <button
+                                className="px-2 py-1 text-xs text-white font-semibold rounded bg-red-600 hover:bg-red-700"
+                                //onClick={() => handleDeleteExpression(expression.id)}
+                                disabled={loading}>
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                ) : (
+                  <p className="text-neutral-300">Nenhuma expressão associada</p>
+                )}
               </div>
             )}
           </>
